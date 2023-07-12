ece955afc6388cc590d3aff815d7aee3eb984df4
AkbarKharismaF <fahri.akbar4281@gmail.com>
2023-07-12 17:51:25 +0700

    UAS PBO

--- Main.java
+++ Main.java
@@ -3,15 +3,19 @@ public class Main {
   public static void main(String[] args) {
     Buku buku1 = new Buku("B001", "Pemrograman java");
     Buku buku2 = new Buku("B002", "Pemrograman C++");
-    BukuLuar buku3 = new BukuLuar("B003", "Pemrograman PHP");
+    BukuLuar buku3 = new BukuLuar("B003", "Harry Potter");
 
-    Petugas petugas1 = new Petugas("Budi", "B123");
+    Petugas petugas1 = new Petugas("Alex", "P001");
     Peminjam peminjam1 = new Mahasiswa();
-    peminjam1.setNamaPeminjam("Alex");
-    peminjam1.setKodePeminjam("B123");
+    peminjam1.setNamaPeminjam("Budi");
+    peminjam1.setKodePeminjam("M001");
+
+    Peminjam peminjam2 = new Mahasiswa();
+    peminjam2.setNamaPeminjam("Cinta");
+    peminjam2.setKodePeminjam("M002");
 
     peminjam1.doLogin();
-
+    peminjam2.doLogin();
     try {
       // Peminjaman peminjaman1 = new Peminjaman(peminjam1, petugas1, buku1);
       // Peminjaman peminjaman1 = new Peminjaman(peminjam1, petugas1, buku2);
@@ -23,7 +27,7 @@ public class Main {
       System.out.println("Nama Petugas :" + peminjaman1.getPetugas().getNamaPetugas());
       System.out.println("Nama Buku :" + peminjaman1.getBuku().getNamaBuku());
 
-      Denda denda2 = new Denda(peminjaman1, 0);
+      Denda denda2 = new Denda(peminjaman1, 7);
       System.out.println("Tagihan denda:" + denda1.getTagihan());
 
       System.out.println("======= Denda dihapuskan ! =======");
